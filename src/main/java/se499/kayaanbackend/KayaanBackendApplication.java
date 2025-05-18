4402613bed6a194fe4a606a18995d18a0b689537
yosakiva <jakkapan_longlai@cmu.ac.th>
2025-05-18 18:26:27 +0700

    First commit

---
+++ src/main/java/se499/kayaanbackend/KayaanBackendApplication.java
@@ -0,0 +1,13 @@
+package se499.kayaanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayaanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KayaanBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se499/kayaanbackend/KayaanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se499.kayaanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayaanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
