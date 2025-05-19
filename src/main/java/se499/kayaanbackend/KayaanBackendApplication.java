17ca67ca62c73414f505dcfb0a83590588ac5ead
Wasan Renumat <wasanrenumat@Wasans-MacBook-Pro-7.local>
2025-05-19 09:19:33 -0400

    minor setup change

--- src/main/java/se499/kayaanbackend/KayaanBackendApplication.java
+++ src/main/java/se499/kayaanbackend/KayaanBackendApplication.java
@@ -8,39 +8,11 @@ import se499.kayaanbackend.entity.Employee;
 import se499.kayaanbackend.repository.EmployeeRepository;
 
 @SpringBootApplication
-public class KayaanBackendApplication implements CommandLineRunner {
+public class KayaanBackendApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(KayaanBackendApplication.class, args);
     }
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        Employee employee1 = Employee.builder()
-                .firstName("John")
-                .lastName("Doe")
-                .email("john.doe@example.com")
-                .build();
-
-        Employee employee2 = Employee.builder()
-                .firstName("Jane")
-                .lastName("Smith")
-                .email("jane.smith@example.com")
-                .build();
-
-        Employee employee3 = Employee.builder()
-                .firstName("Alex")
-                .lastName("Johnson")
-                .email("alex.johnson@example.com")
-                .build();
-
-        employeeRepository.save(employee1);
-        employeeRepository.save(employee2);
-        employeeRepository.save(employee3);
-    }
+   
 }
