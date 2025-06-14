a8dd4c57f0ef9ea2db13375df112a942b6120bbb
Wasan Renumat <wasanrenumat@Wasans-MacBook-Pro-7.local>
2025-06-14 12:06:53 -0400

    Feat: Avatar can be uploaded or saved avatar

--- src/main/java/se499/kayaanbackend/security/auth/AuthenticationResponse.java
+++ src/main/java/se499/kayaanbackend/security/auth/AuthenticationResponse.java
@@ -1,3 +1,4 @@
+// AuthenticationResponse.java
 package se499.kayaanbackend.security.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -5,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import se499.kayaanbackend.security.user.User;
 
 @Data
 @Builder
@@ -17,4 +19,29 @@ public class AuthenticationResponse {
   @JsonProperty("refresh_token")
   private String refreshToken;
   private String message;
+  private UserInfo user;
+
+  @Data
+  @Builder
+  @AllArgsConstructor
+  @NoArgsConstructor
+  public static class UserInfo {
+    private Integer id;
+    private String username;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String avatarUrl;
+
+    public static UserInfo fromUser(User user) {
+      return UserInfo.builder()
+              .id(user.getId())
+              .username(user.getUsername())
+              .email(user.getEmail())
+              .firstname(user.getFirstname())
+              .lastname(user.getLastname())
+              .avatarUrl(user.getAvatarUrl())
+              .build();
+    }
+  }
 }
