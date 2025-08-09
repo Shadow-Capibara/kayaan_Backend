f9bacaf0e47a71294f444b387271516aeb84c4f9
SammyEiei <wasanrenumat@Wasans-MacBook-Pro-7.local>
2025-08-09 17:52:58 +0700

    chore: finalize configs/imports; E2E skeleton ready for local dev

---
+++ src/test/java/com/kayaan/basic/SmokeTest.java
@@ -0,0 +1,13 @@
+package com.kayaan.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = se499.kayaanbackend.KayaanBackendApplication.class)
+class SmokeTest {
+    @Test
+    void contextLoads() {
+    }
+}
+
+
